298a3e67409a43b0ba25ea005f6820449ccf5cbb
Matt Glover <matt@duchysoftware.com>
2014-01-14 14:48:16 +0000

    Added Kiwi Pod

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '7.0'
+
+  target :BotDemoTests, :exclusive => true do
+      pod 'Kiwi/XCTest'
+  end
---
+++ Pods/Pods-BotDemoTests-Kiwi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BotDemoTests_Kiwi : NSObject
+@end
+@implementation PodsDummy_Pods_BotDemoTests_Kiwi
+@end
